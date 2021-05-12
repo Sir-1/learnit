d715c018e0dd34bd3b516d193b41ca327b444261
matthew <matthew.jamesm001@gmail.com>
2021-05-12 14:30:46 +1200

    started working on classroom pages

--- main.py
+++ main.py
@@ -19,5 +19,23 @@ def main():
                            classrooms=classes)
 
 
+@app.route("/classroom/<ClassID>")
+def classrooms(ClassID):
+    conn = sqlite3.connect("learnit.db")
+    cursor = conn.cursor()
+    cursor.execute("""SELECT Post.id,Post.title, Post.content,
+                            Post.Text, Post.Usefulness, User.name,
+                            classroom.name from Post join user ON Post.Uid =
+                            User.id join classroom ON Post.Cid = classroom.id
+                            where cid = ?;
+                            """, (ClassID,))
+    posts = cursor.fetchall()
+    classroom = cursor.execute("""select * from classroom where id =
+                               ?""", (ClassID,))
+    classroom = cursor.fetchone()
+    print(classroom, ClassID)
+    return render_template("classroom.html", stuff=posts, info=classroom)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
