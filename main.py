1070018c599a778427beb90d12faa57ce9c45ef1
matthew <matthew.jamesm001@gmail.com>
2021-05-21 10:25:52 +1200

    title is now a link sql queries now in function

--- main.py
+++ main.py
@@ -4,36 +4,40 @@ from flask import Flask, Response, render_template, abort, request
 app = Flask(__name__)
 
 
+def do_query(query, data, fetch):
+    conn = sqlite3.connect("learnit.db")
+    cur = conn.cursor()
+    cur.execute(query, data)
+    if fetch is None:
+        result = cur.fetchall()
+    else:
+        result = cur.fetchone()
+    conn.close()
+    return result
+
+
 @app.route("/")
 def main():
-    conn = sqlite3.connect("learnit.db")
-    cursor = conn.cursor()
-    posts = cursor.execute("""SELECT Post.id,Post.title, Post.content,
+    posts = do_query("""SELECT Post.id,Post.title, Post.content,
                             Post.Text, Post.Usefulness, User.name,
                             classroom.name from Post join user ON Post.Uid =
                             User.id join classroom ON Post.Cid = classroom.id;
-                            """)
-    posts = cursor.fetchall()
-    classes = cursor.execute("select name from classroom;")
+                            """, (), None)
+    print(posts)
+    classes = do_query("select name from classroom;", (), None)
     return render_template("main.html", title="main", stuff=posts,
                            classrooms=classes)
 
 
 @app.route("/classroom/<ClassID>")
 def classrooms(ClassID):
-    conn = sqlite3.connect("learnit.db")
-    cursor = conn.cursor()
-    cursor.execute("""SELECT Post.id,Post.title, Post.content,
+    posts = do_query("""SELECT Post.id,Post.title, Post.content,
                             Post.Text, Post.Usefulness, User.name,
                             classroom.name from Post join user ON Post.Uid =
                             User.id join classroom ON Post.Cid = classroom.id
                             where cid = ?;
-                            """, (ClassID,))
-    posts = cursor.fetchall()
-    classroom = cursor.execute("""select * from classroom where id =
-                               ?""", (ClassID,))
-    classroom = cursor.fetchone()
-    print(classroom, ClassID)
+                            """, (ClassID,), None)
+    classroom = do_query("select * from classroom where id=?", (ClassID,), 1)
     return render_template("classroom.html", stuff=posts, info=classroom)
 
 
