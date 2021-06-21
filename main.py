740b15dac83cbb2d34307237a11bf9da4fb6557f
matthew <matthew.jamesm001@gmail.com>
2021-06-22 09:36:20 +1200

    made alot of hcanges catching everything up to current build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import (Flask, Response, render_template, abort, request, session, redirect, url_for)
+from flask import (Flask, Response, render_template, abort, request, session, redirect, url_for,flash)
 
 app = Flask(__name__)
 app.secret_key = """b'(\x9a^\xde\xf8tZm_/&?X\xeb\x00\xda'"""
@@ -29,7 +29,7 @@ def main_user():
                         Post.Text, Post.Usefulness, User.name,
                         classroom.name,Cid from Post join user ON
                         Post.Uid=User.id join classroom ON Post.Cid =
-                        classroom.id;""", (), None)
+                        classroom.id order by Post.id DESC;""", (), None)
         classes = do_query("""SELECT name, id from classroom""", (), None)
 # if there is no user logged in fetch all the
 # general information from the database
@@ -41,8 +41,17 @@ def main_user():
                         Join User_Classroom ON User.id = User_Classroom.Uid
                         JOIN classroom ON User_Classroom.Cid = classroom.id
                         JOIN Post ON classroom.id = Post.Cid
-                        WHERE User.id = ?""",
+                        WHERE User.id = ? order by Post.id DESC""",
                          (int(session["_User"]),), None)
+        names = do_query("""SELECT User.name from Post join User on User.id =
+                            Post.id WHERE Post.Cid in
+                            (SELECT Cid from User_Classroom Where Uid = ?)""",
+                         (session["_User"],), "")
+        try:
+            for i in range(len(names)):
+                posts[i][5] = names[i]
+        except Exception as error:
+            print(error)
         classes = do_query("""select classroom.name, id from User_Classroom
                             join classroom ON classroom.id = Cid WHERE Uid = ?;
                             """, (int(session["_User"]),), None)
@@ -56,23 +65,26 @@ def main_user():
 @app.route("/classroom/<ClassID>")
 def classrooms(ClassID):
     name = ''
+    id = 0
     # collect all of the posts within a classroom and open the classsroom page
     posts = do_query("""SELECT Post.id,Post.title, Post.content,
                             Post.Text, Post.Usefulness, User.name,
                             classroom.name,Cid from Post join user ON Post.Uid
                             = User.id join classroom ON Post.Cid = classroom.id
-                            where cid = ?;
+                            where cid = ? order by Post.id DESC;
                             """, (ClassID,), None)
     classroom = do_query("select * from classroom where id=?", (ClassID,), 1)
     classes = do_query("select name, id from classroom;", (), None)
-    name = do_query("SELECT name from User where id = ?",
-                    (session["_User"],), "fetch")
+    if session.get("_User") is not None:
+        id = session["_User"]
+        name = do_query("SELECT name from User where id = ?",
+                        (session["_User"],), "fetch")
     return render_template("classroom.html", stuff=posts, info=classroom,
                            classrooms=classes, user_name=name,
-                           uid=session.get("_User"))
+                           uid=id)
 
 
-@app.route("/login", methods=["POSt"])
+@app.route("/login", methods=["POST"])
 def login():
     name, password = "", ""
     name, password = str(request.form.get("UserName")), str(request.form.get("Password"))
@@ -80,9 +92,55 @@ def login():
                     (name,), 'hi')
     print(info)
     if info is not None:
-        if info[2] == password:
+        if str(info[2]) == str(password):
             session["_User"] = info[0]
     return redirect(url_for("main_user"))
+
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    session.pop("_User")
+    return redirect(url_for("main_user"))
+
+
+@app.route("/classes")
+def view_classess():
+    name = ''
+    my_classess = ""
+    id = 0
+# if there is a user signed in fetch their information feom the database
+    if session.get("_User") is None:
+        classes = do_query("""SELECT name, id from classroom""", (), None)
+# if there is no user logged in fetch all the
+# general information from the database
+    else:
+        id = session["_User"]
+        classes = do_query("""select classroom.name, id from User_Classroom
+                            join classroom ON classroom.id = Cid WHERE Uid = ?;
+                            """, (int(session["_User"]),), None)
+        name = do_query("SELECT name from User where id = ?",
+                        (session["_User"],), "fetch")
+        my_classess = do_query("""SELECT id from classroom where id in (select Cid from User_Classroom where Uid = ?)""", (session["_User"],), None)
+        print(my_classess)
+    posts = do_query("SELECT * from classroom", (), None)
+    return render_template("ClassMenu.html", title="Classes", stuff=posts,
+                           classrooms=classes, uid=id, user_name=name, myclassrooms=my_classess)
+
+
+@app.route("/join_Classroom", methods=["POST"])
+def join_class():
+    my_classess = do_query("""SELECT id from classroom where id in (select Cid from User_Classroom where Uid = ?)""", (session["_User"],), None)
+    conn = sqlite3.connect("learnit.db")
+    cur = conn.cursor()
+    room = request.form.get("Join")
+    if (int(room),) not in my_classess:
+        cur.execute("""INSERT Into User_Classroom (Uid,Cid,Admin)
+                    Values(?,?,0)""", (str(session["_User"]), str(room)))
+    else:
+        cur.execute("""DELETE FROM User_Classroom where Uid = ? and Cid = ?""", (session["_User"], room))
+    conn.commit()
+    conn.close()
+    return redirect(url_for("view_classess"))
 
 
 if __name__ == "__main__":
